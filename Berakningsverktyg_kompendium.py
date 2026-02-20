953c2dbe93125d3bd6c8d04d6155e06b60295ace
Floffan <filippa.rolfsman@gmail.com>
2026-02-20 12:12:49 +0100

    Början på själva kompendiet, olika delar

--- Berakningsverktyg_kompendium.py
+++ Berakningsverktyg_kompendium.py
@@ -3,17 +3,39 @@ import sympy as sp
 import numpy as np
 
 tal = 9
-
+# %% Default: Användbara kommandon
 """
-Default: Användbara kommandon
+Beskrivning
 """
 modulus = 9%2 # ger resten av 2 hela delningar av 9
 
+# %% Numpy: Användbara kommandon
 """
-Numpy: Användbara kommandon
+Från numpy
 """
 
+array = np.array([2,5,7,3])
+a2 = np.array([1,1,1,1,1,1])
+
+# tar medelvärdet ur listan
+medelvärde = np.average(array) 
+
+# räknar ut standardavvikelsen med hjälp av np.sum()
+standardavvikelse = np.sqrt(np.sum(((medelvärde)**2)/len(array))) 
+
+# roten ur ett tal
 roten_ur = np.sqrt(tal)
+
+# np.pad "fyller ut" den kortare listan med nollor så att listorna blir lika långa
+p1 = np.pad(array, (0,len(a2)-len(array)))
+
+
+
+
+
+
+
+
 
 x = np.arange(2019,2026)
 #x = [2019, 2022, 2023, 2025]
